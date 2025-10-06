e1242028843bf07a677c1e50afd2a9d3cf7a26b1
Anthony Arnold <stubbiegaming@gmail.com>
2025-10-06 15:48:26 -0400

    prevent globals rescanning

--- src/backend/cpp/gc/src/runtime/memory/gc.cpp
+++ src/backend/cpp/gc/src/runtime/memory/gc.cpp
@@ -340,13 +340,14 @@ static void checkPotentialPtr(void* addr, BSQMemoryTheadLocalInfo& tinfo) noexce
 
 static void walkStack(BSQMemoryTheadLocalInfo& tinfo) noexcept 
 {
-    // Process global data (TODO -- later have flag to disable this after it is fixed as immortal)
-    if(GlobalDataStorage::g_global_data.native_global_storage != nullptr) {
+    if(GlobalDataStorage::g_global_data.needs_scanning) {
         void** curr = GlobalDataStorage::g_global_data.native_global_storage;
         while(curr < GlobalDataStorage::g_global_data.native_global_storage_end) {
             checkPotentialPtr(*curr, tinfo);
             curr++;
         }
+
+        GlobalDataStorage::g_global_data.needs_scanning = false;
     }
 
 #ifdef BSQ_GC_CHECK_ENABLED
