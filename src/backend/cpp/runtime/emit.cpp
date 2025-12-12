c0fb465d9a61cc7381886d50e71761fb288be8b5
Anthony Arnold <stubbiegaming@gmail.com>
2025-12-12 12:06:20 -0500

    some logging and more testing

--- src/backend/cpp/runtime/emit.cpp
+++ src/backend/cpp/runtime/emit.cpp
@@ -11,22 +11,41 @@ int wrap_setjmp() {
         return EXIT_FAILURE;
     }
 
+    std::cerr << "Begin init gc\n";
+
     gtl_info.initializeGC<sizeof(allocs) / sizeof(allocs[0])>(allocs);
+
+    std::cerr << "GC init done\nBegin init decs object\n";
+
     gtl_info.decs.initialize(&gtl_info);
+
+    std::cerr << "Decs init done\nRun main\n";
 
     // Calling our emitted main is hardcoded for now
     __CoreCpp::MainType ret = Main::main();
     std::cout << __CoreCpp::to_string(ret) << std::endl;
 
+    std::cerr << "Main finished\n";
+
     // Ensure decs thread stops waiting
     gtl_info.decs.signalFinished();
+
+    std::cerr << "Decs thread completed\n\n";
 
     return 0;
 }
 
 int main() {
+
+    std::cerr << "Initializing locks\n";
+
     INIT_LOCKS();   
+
+    std::cerr << "Locks initialized\n";
+
     InitBSQMemoryTheadLocalInfo();
+
+    std::cerr << "BSQTheadLocalInfo init\n";
 
     return wrap_setjmp();
 }
